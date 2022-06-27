086bd6e24dd97e42f6f8ccb066ed374fd1a0e5fe
joethyr <joethayr@gmail.com>
2022-06-27 10:18:05 -0600

    update navbar links and its layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
