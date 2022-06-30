340135f57a95f15bc1be8475d51edc4291a2259a
joethyr <joethayr@gmail.com>
2022-06-30 13:07:44 -0600

    migrate Comment table to database and include user and picture references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_213009) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_30_190453) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_213009) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "reply"
+    t.integer "picture_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_comments_on_picture_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "impressions", force: :cascade do |t|
@@ -105,5 +116,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_213009) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "pictures"
+  add_foreign_key "comments", "users"
   add_foreign_key "pictures", "users"
 end
